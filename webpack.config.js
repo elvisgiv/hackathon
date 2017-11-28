9a48417c8513c306ccb71b91752963678d89b748
Dmitry <jesusinyourtown@gmail.com>
2017-11-28 12:26:37 +0200

    scss support

--- webpack.config.js
+++ webpack.config.js
@@ -36,7 +36,19 @@ function loaders() {
             test: /\.(jpg|png|svg)$/,
             loader: 'file-loader',
             exclude: /node_modules/
+        },
+
+        {
+            test: /\.scss$/,
+            use: [{
+                loader: "style-loader" // creates style nodes from JS strings
+            }, {
+                loader: "css-loader" // translates CSS into CommonJS
+            }, {
+                loader: "sass-loader" // compiles Sass to CSS
+            }]
         }
+
     ]
 }
 
