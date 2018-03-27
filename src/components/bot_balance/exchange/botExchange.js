a28110d99f3e6854664dbf4602327802eba8993a
Dmitry <jesusinyourtown@gmail.com>
2018-03-27 19:48:00 +0300

    wallet page in progress

--- src/components/bot_balance/exchange/botExchange.js
+++ src/components/bot_balance/exchange/botExchange.js
@@ -1,106 +1,150 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
-import { Button } from 'rmwc/Button';
-//
+
 import ReturnEth from "./returnEth";
 import ReturnSkl from "./returnSkl";
-//
-const gex = require('@skale-labs/skale-api');
+
+const skale = require('@skale-labs/skale-api');
+
+import {Button} from 'rmwc/Button';
+const Identicon = require('identicon.js');
 
 
 export default class BotExchange extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            skl: '',
-            eth: '',
-        };
-        //
-        this.checkBalances =this.checkBalances.bind(this)
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
 
-    componentWillReceiveProps(){
-        this.setState({web3Connector: this.props.web3Connector});
+  componentWillReceiveProps() {
+    if (!this.state.libInit && this.props.web3Connector) {
+      let provider = this.props.web3Connector.provider;
+      skale.initBothProviders('51.0.1.99', '8546', provider);
+      this.setState({libInit: true});
+      this.initBalanceChecker();
+      this.initAvatarData();
+    }
+  }
+
+  initBalanceChecker() {
+    this.checkBalances();
+    this.setState({
+      timer: setInterval(() => {
         this.checkBalances()
-    }
+      }, 5000),
+    });
+  }
 
-    // balance
-    async checkBalances() {
-        // from metamask
-        let accounts = await gex.w3.getAccounts();
-        // in wei
-        let firstAccountBalance = await gex.token().balanceOf(accounts[0]);
-        let firstAccountEthBalance = await gex.w3.web3.eth.getBalance(accounts[0]);
-        //
-        this.setState({
-            skl: gex.w3.web3.utils.fromWei(firstAccountBalance),
-            eth: gex.w3.web3.utils.fromWei(firstAccountEthBalance)
-        });
+  async checkBalances() {
+    let accounts = await skale.w3.getAccounts();
+    let account = accounts[0];
+    let skaleBotAccountInfo = await skale.bot().getInfoForAccount(account);
 
-        // from skalebot
-        let accountInfo = await gex.bot().getInfoForAccount(accounts[0]);
-        //
-        this.setState({
-            botEth: gex.w3.web3.utils.fromWei(accountInfo.sendEth),
-            botSkale: gex.w3.web3.utils.fromWei(accountInfo.sendGex),
-        });
-    }
+    let accountSkaleWeiBalance = await skale.token().balanceOf(account);
+    let accountSkaleBalance = skale.w3.web3.utils.fromWei(accountSkaleWeiBalance.toString());
 
+    let accountEthWeiBalance = await skale.w3.web3.eth.getBalance(account);
+    let accountEthBalance = skale.w3.web3.utils.fromWei(accountEthWeiBalance.toString());
 
-    ///////////////////////////
-    render(){
+    let skaleBotSkaleBalance = skale.w3.web3.utils.fromWei(skaleBotAccountInfo.sendGex.toString());
+    let skaleBotEthBalance = skale.w3.web3.utils.fromWei(skaleBotAccountInfo.sendEth.toString());
 
-        let returnEth = <ReturnEth web3Connector={this.state.web3Connector}/>;
-        let returnSkl = <ReturnSkl web3Connector={this.state.web3Connector}/>;
+    this.setState({
+      account: account,
+      skl: accountSkaleBalance,
+      eth: accountEthBalance,
+      botEth: skaleBotEthBalance,
+      botSkale: skaleBotSkaleBalance
+    });
+  }
 
-        return(
-            <div className='marg-30'>
+  async initAvatarData() {
+    let accounts = await skale.w3.getAccounts();
+    let account = accounts[0];
+    this.setState({
+      avatarData: new Identicon(account, {margin: 0.2, size: 35}).toString()
+    });
 
-                <div className="mdc-elevation--z4 padd-top-10 padd-30"  style={{maxWidth: '550px'}}>
-                    <div>
-                        <h4 className="bold no-marg" >MetaMask:</h4>
-                        <br/>
-                        <span style={{'fontWeight':'bold'}}>ETH: </span>
-                        {this.state.eth}
-                        <br/>
-                        <Link to='/exchange-eth' className="undec">
-                            <Button dense>Sell SKL</Button>
-                        </Link>
-                        <br/>
-                        <br/>
-                        <span style={{'fontWeight':'bold'}}>SKL: </span>
-                        {this.state.skl}
-                        <br/>
-                        <Link to='/exchange-skl' className="undec">
-                            <Button dense>Buy SKL</Button>
-                        </Link>
-                    </div>
-                </div>
-                <br/>
+  }
 
-                <div className="mdc-elevation--z4 padd-top-10 padd-30"  style={{maxWidth: '550px'}}>
-                    <div>
-                        <h4 className="bold no-marg" >SkaleBot:</h4>
-                        <br/>
-                        <span style={{'fontWeight':'bold'}}>ETH:</span>
-                        {this.state.botEth}
-                        <br/>
-                        {returnEth}
-                        <br/>
-                        <br/>
-                        <span style={{'fontWeight':'bold'}}>SKL:</span>
-                        {this.state.botSkale}
-                        <br/>
-                        {returnSkl}
-                    </div>
-                </div>
+  render() {
 
+    let returnEth = <ReturnEth web3Connector={this.state.web3Connector}/>;
+    let returnSkl = <ReturnSkl web3Connector={this.state.web3Connector}/>;
+
+    return (
+      <div className='marg-30'>
+        <div className="skale-card mdc-elevation--z4" style={{maxWidth: '750px'}}>
+          <div>
+            <div className="fl-cont fl-center-vert card-top">
+              <div className="fl-col padd-ri-10">
+                {this.state.avatarData ? <img width={35} height={35} style={{borderRadius: "10px"}} src={"data:image/png;base64,"+this.state.avatarData}/> : null}
+              </div>
+              <div className="fl-col">
+                <h6 className="bold no-marg">{this.state.account}</h6>
+              </div>
             </div>
 
 
-        )
+            <div className="card-content padd-30">
 
-    }
+              <div className="fl-cont fl-center-vert">
+                <div className="fl-col padd-ri-10">
+                  {this.state.avatarData ? <img width={35} height={35} style={{borderRadius: "10px"}} src={"data:image/png;base64,"+this.state.avatarData}/> : null}
+                </div>
+                <div className="fl-col">
+                  <h6 className="no-marg">{this.state.eth} ETH </h6>
+                </div>
+              </div>
+
+
+              <span style={{fontWeight: 'bold'}}>ETH: </span>
+              {this.state.eth}
+              <br/>
+              <Link to='/exchange-eth' className="undec">
+                <Button dense>Sell SKL</Button>
+              </Link>
+              <br/>
+              <br/>
+              <span style={{fontWeight: 'bold'}}>SKL: </span>
+              {this.state.skl}
+              <br/>
+              <Link to='/exchange-skl' className="undec">
+                <Button dense>Buy SKL</Button>
+              </Link>
+            </div>
+
+          </div>
+        </div>
+        <br/>
+
+        <div className="skale-card mdc-elevation--z4" style={{maxWidth: '750px'}}>
+          <div className="fl-cont fl-center-vert card-top">
+            <div className="fl-col">
+              <h6 className="bold no-marg">Skale Bot</h6>
+            </div>
+          </div>
+          <div className="padd-30">
+            <span style={{fontWeight: 'bold'}}>ETH:</span>
+            {this.state.botEth}
+            <br/>
+            {returnEth}
+            <br/>
+            <br/>
+            <span style={{fontWeight: 'bold'}}>SKL:</span>
+            {this.state.botSkale}
+            <br/>
+            {returnSkl}
+          </div>
+        </div>
+
+      </div>
+
+
+    )
+
+  }
 
 }
