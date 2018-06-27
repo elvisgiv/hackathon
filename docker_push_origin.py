0ea44a85fd42a478f19147b0d7dc22e9c97fcbcb
elvisgiv <alexeymaratovich@gmail.com>
2018-06-27 13:07:45 +0300

    test python commit

---
+++ docker_push_origin.py
@@ -0,0 +1,12 @@
+from git import Repo,remote
+
+rw_dir = '/mnt/elvisdata/work/exchange-ui'
+repo = Repo(rw_dir)
+
+add = repo.git.add(".")
+
+commit =  repo.git.commit("-m test python commit")
+
+
+origin = repo.remote(name='origin')
+origin.push()
