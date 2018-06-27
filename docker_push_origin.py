0dad80c405415cbcd99a22021918a2d842651bf5
elvisgiv <alexeymaratovich@gmail.com>
2018-06-27 13:12:12 +0300

    add commit name to python script

--- docker_push_origin.py
+++ docker_push_origin.py
@@ -1,12 +1,13 @@
 from git import Repo,remote
 
+commit_name = raw_input("Enter Commit name: ")
+
 rw_dir = '/mnt/elvisdata/work/exchange-ui'
 repo = Repo(rw_dir)
 
 add = repo.git.add(".")
 
-commit =  repo.git.commit("-m", "test python commit")
-
+commit =  repo.git.commit("-m", commit_name)
 
 origin = repo.remote(name='origin')
 origin.push()
